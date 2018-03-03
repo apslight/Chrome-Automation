3f08f1d610b2d634cbc22f06d0cd14c141d8154d
apsNazii <adityapratap.singh28@gmail.com>
2018-03-03 16:16:06 +0530

    Send mail via way2sms

---
+++ way2sms.py
@@ -0,0 +1,48 @@
+import getpass
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+mobileStr = raw_input('Email: ')
+passwordStr = getpass.getpass()
+tomobile = raw_input('To_mobile')
+totext = raw_input('To_Text')
+
+browser = webdriver.Chrome()
+browser.get(('http://site24.way2sms.com/content/index.html?'))
+
+username = browser.find_element_by_id('username')
+username.send_keys(mobileStr)
+
+password = browser.find_element_by_id('password')
+password.send_keys(passwordStr)
+
+nextButton = browser.find_element_by_id('loginBTN')
+nextButton.click()
+
+sendsms = WebDriverWait(browser, 5).until(
+    EC.presence_of_element_located((By.ID, 'sendSMS')))
+sendsms.click()
+
+browser.switch_to_frame(browser.find_element_by_id("frame"))
+
+mobiletext = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.ID, 'mobile')))
+mobiletext.send_keys(tomobile)
+
+text = WebDriverWait(browser, 10).until(
+    EC.presence_of_element_located((By.ID,'message')))
+text.send_keys(totext)
+
+bhejosms = WebDriverWait(browser, 5).until(
+    EC.presence_of_element_located((By.ID, 'Send')))
+bhejosms.click()
+
+
+browser.quit()
+
+
+
+
+
